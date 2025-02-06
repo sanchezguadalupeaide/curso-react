05d6e0af3e4d956ab74721b5f32e78fce28f9a28
sanchezguadalupeaide <sanchez.guadalupeaide@iesconcepcion.edu.ar>
2025-02-05 21:14:22 -0300

    Agregando componentes AddTask

--- src/componentes/Logos.jsx
+++ src/componentes/Logos.jsx
@@ -1,7 +1,7 @@
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 
-const Logos = () => {
+export const Logos = () => {
     return <div>
             <a href="https://vite.dev" target="_blank">
               <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -12,4 +12,3 @@ const Logos = () => {
           </div>
 }
 
-export default Logos
